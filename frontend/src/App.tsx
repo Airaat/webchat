b81b9a64b2b373c94643fafe01d81e1f0f832eb0
John Doe <johndoe@mail.com>
2026-03-09 22:58:39 +0300

    deps | Zustand and TanStack Query installation --- Added new dependencies for client and server state management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,28 @@
 import {ThemeProvider} from '@mui/material/styles';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {AuthProvider} from './contexts/AuthContext';
 import {LoginPage} from './pages/auth/LoginPage';
 import {SignUpPage} from "./pages/auth/SignUpPage";
 import {ChatPage} from './pages/chat/ChatPage';
 import {theme} from './styles/theme';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <AuthProvider>
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<LoginPage/>}/>
-                        <Route path="/login" element={<LoginPage/>}/>
-                        <Route path="/signup" element={<SignUpPage/>}/>
-                        <Route path="/chat" element={<ChatPage/>}/>
-                    </Routes>
-                </Router>
-            </AuthProvider>
-        </ThemeProvider>
+        <QueryClientProvider client={new QueryClient}>
+            <ThemeProvider theme={theme}>
+                <AuthProvider>
+                    <Router>
+                        <Routes>
+                            <Route path="/" element={<LoginPage/>}/>
+                            <Route path="/login" element={<LoginPage/>}/>
+                            <Route path="/signup" element={<SignUpPage/>}/>
+                            <Route path="/chat" element={<ChatPage/>}/>
+                        </Routes>
+                    </Router>
+                </AuthProvider>
+            </ThemeProvider>
+        </QueryClientProvider>
     );
 }
 
